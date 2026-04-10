f41101986b86e3fb8ab6c210ae74cd077cb68e6b
Andrius Paurys <andrius@paurys.lt>
2026-04-10 22:33:22 +0300

    fix: update DC motor pins for new hardware

--- src/Stumikas/config-pins.h
+++ src/Stumikas/config-pins.h
@@ -7,14 +7,16 @@
 
 
 // LED matrix DI pin
-#define LED_PIN       GPIO_NUM_15
+#define LED_PIN           GPIO_NUM_15
 
 // Servo motor control pins
-#define SERVO1_PIN    GPIO_NUM_1
-#define SERVO2_PIN    GPIO_NUM_14
+#define SERVO1_PIN        GPIO_NUM_1
+#define SERVO2_PIN        GPIO_NUM_14
 
 // DC motor pins
-#define MOTOR_1A_PIN  GPIO_NUM_18
-#define MOTOR_1B_PIN  GPIO_NUM_40
-#define MOTOR_2A_PIN  GPIO_NUM_17
-#define MOTOR_2B_PIN  GPIO_NUM_21
+#define MOTOR_LEFT_PIN1   GPIO_NUM_14
+#define MOTOR_LEFT_PIN2   GPIO_NUM_13
+#define MOTOR_RIGHT_PIN1  GPIO_NUM_12
+#define MOTOR_RIGHT_PIN2  GPIO_NUM_11
+
+#define MOTOR_ENABLE_PIN  GPIO_NUM_38
